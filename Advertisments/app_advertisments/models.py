bb150a689aad02704c434239933895497c180086
Greev50 <Greev50@bk.ru>
2023-08-21 22:36:02 +0300

    dz - 7

--- Advertisments/app_advertisments/models.py
+++ Advertisments/app_advertisments/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Advertisment(models.Model):
+    class Meta: 
+        db_table = 'advertisements'
+    def __str__(self): 
+        return f'<Advertisement: Advertisement(id={self.id}, title={self.title}, price={self.price})>'
+    title = models.CharField("Название", max_length=120)
+    descriptions = models.TextField("Описание")
+    price = models.DecimalField("Цена", max_digits=9, decimal_places=2)
+    trades = models.BooleanField("Торг", help_text = "Если хотим торговаться")
+    date_now = models.DateField("Создание дата", auto_now_add=True)
+    date_update = models.DateField("Обновление дата", auto_now=True)
+    pass
